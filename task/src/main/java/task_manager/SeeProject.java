a8175aaa4acf826fe43acc07a3d0211b5acd7bcc
Stefan <Stefan@Stefan-PC>
2016-07-14 10:54:46 +0300

    Commit next

---
+++ task/src/main/java/task_manager/SeeProject.java
@@ -0,0 +1,33 @@
+package task_manager;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class SeeProject extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		HttpSession session = req.getSession(false);
+		
+		if(session.getAttribute("firstname")!=null)
+			
+				{	req.setAttribute("firstname", req.getParameter("firstname"));
+					req.getRequestDispatcher("/jsp/SeeProject.jsp").forward(req, resp);	
+					}
+			
+			else
+				{	req.getRequestDispatcher("/jsp/login.jsp").forward(req, resp);	}
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
---
+++ task/src/main/webapp/jsp/SeeProject.jsp
@@ -0,0 +1,27 @@
+<html>
+<head>
+	<title>Projects</title>
+	<style> <jsp:directive.include file="css/style.jsp"/> </style>
+</head>
+
+<body>
+	<div id="header">
+			 <h2>Hello <%= (String)session.getAttribute("firstname") %></h2>
+   			  <h5>id sesion: <% out.print( session.getId()); %></h5>
+	</div>
+	
+	<div id="meniu">
+   			 <jsp:directive.include file="menus/Meniu.jsp" />
+	</div> 
+	<div id="sec">
+		<div id="secundar">
+				 <jsp:directive.include file="menus/MeniuProject.jsp" />	
+		</div>
+	
+		<div id="create">
+			
+			
+		</div>
+	</div>
+</body>
+</html>
